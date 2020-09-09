125565bb79cfbce61b91f6c384b57b7112b0448c
Alex Konas <alex.konas@menloschool.org>
2020-09-09 15:16:35 -0700

    basic ghost game with two human players

---
+++ word_games.py
@@ -0,0 +1,22 @@
+min_word_length = 4
+scrabble_words = []
+with open("scrabble.txt", "r") as f:
+	for word in f:
+		scrabble_words.append(word[:-1])
+# print(scrabble_words)
+
+def get_letter():
+	return input("Enter a letter: ")
+
+def if_over(word):
+	if word in scrabble_words:
+		return True
+	return False
+
+def play_ghost():
+	current = get_letter()
+	while len(current) < min_word_length and current not in scrabble_words:
+		current = current + get_letter()
+	print(current)
+
+play_ghost()
